6f9ec8c9de48c588ea57a7fdd83d993451e3d5fa
Sandeep Vantipalli <deepkeer@Sandeeps-MacBook-Air.local>
2026-09-22 16:03:28 +0530

    Fix EKS access policy dependency

--- terraform/main.tf
+++ terraform/main.tf
@@ -279,5 +279,9 @@ resource "aws_eks_access_policy_association" "github_actions_access_policy" {
   access_scope {
     type = "cluster"
   }
+
+  depends_on = [
+    aws_eks_access_entry.github_actions_access
+  ]
 }
 
